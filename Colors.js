3436dc7bdab8e8518e112906a554b363dc2caa5d
Caroline <caroline.adao@acct.global>
2020-10-03 15:26:44 -0300

    adding colors

---
+++ Colors.js
@@ -0,0 +1,11 @@
+const colors = {
+  black: "#2D3436",
+  blue: "#02abff",
+  lightBlue: "#cceeff",
+  strongBlue: "#005580",
+  white: "#FFFFFF",
+  grey: "#CACACA",
+  red: "#D85963"
+}
+
+export default colors;
