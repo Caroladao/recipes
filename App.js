e00c15236248a1059355dfb6a4ad722b5f59136e
Caroline <caroline.adao@acct.global>
2020-10-03 15:41:33 -0300

    adding button and changing app name

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 import colors from './Colors';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{flexDirection: "row"}}>
+        <View style={styles.divider} />
+        <Text style={styles.title}>My<Text style={styles.title2}>Recipes</Text></Text>
+        <View style={styles.divider} />
+      </View>
+
+      
+      <TouchableOpacity style={styles.addItem}>
+        <MaterialIcons name="add-circle" size={60} color={colors.blue} />
+      </TouchableOpacity>
     </View>
   );
 }
@@ -19,4 +27,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  divider: {
+    backgroundColor: colors.lightBlue,
+    height: 1,
+    flex: 1,
+    alignSelf: 'center',
+  },
+  title: {
+    fontSize: 38,
+    fontWeight: '300',
+    color: colors.black,
+    paddingHorizontal: 64,
+  },
+  title2: {
+    fontWeight: 'bold',
+    color: colors.blue,
+  },
+  addItem: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    bottom: 15,
+    right: 15
+  }
 });
